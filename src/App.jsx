8b4d6e2788caf48b095256220a079643ef42423d
Jack200133 <car20593@uvg.edu.gt>
2022-05-30 03:05:36 -0600

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Navbar from './componentes/Navbar/Navbar'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './Navbar.css'
+
+function Navbar() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="container">
+            <a href="#"><span>Home</span></a>
+            <a href="#"><span>Portafolio</span></a>
+            <a href="#"><span>Get in contact</span></a>
+        </div>
+    )
+}
+
+export default Navbar
